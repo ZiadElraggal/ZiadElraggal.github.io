135ce6e38b1f9dc7919fc32f99178e2c11fd1953
ZiadElraggal <161850748+ZiadElraggal@users.noreply.github.com>
2024-04-06 14:50:47 -0230

    Video Update for project 3706

--- public/ScriptProjects.js
+++ public/ScriptProjects.js
@@ -11,3 +11,11 @@ if (logo) {
 
 
 });
+document.getElementById("view-project-button").addEventListener("click", function() {
+    document.getElementById("video-modal").style.display = "block";
+  });
+  
+  document.getElementById("close-modal-button").addEventListener("click", function() {
+    document.getElementById("video-modal").style.display = "none";
+    document.getElementById("modal-video").pause();
+  });
